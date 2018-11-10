0f7053742f3c3f77250e80c11adb34db113d0e83
Laurence Hurst <hurstla@bb2login01.bb2.cluster>
2018-11-10 20:32:55 +0000

    Added test with correct values, from online implementation.

--- Testing/testPlayfairCipher.cpp
+++ Testing/testPlayfairCipher.cpp
@@ -6,12 +6,12 @@
 
 TEST_CASE("Playfair Cipher encryption", "[playfair]") {
   PlayfairCipher pc{"Cipher Key"};
-  REQUIRE( pc.applyCipher("HELLOWORLD", CipherMode::Encrypt) == "ECSHSFQVDAUM");
+  REQUIRE( pc.applyCipher("HELLOWORLD", CipherMode::Encrypt) == "ECSHFSVQADMU");
 }
 
 TEST_CASE("Playfair Cipher decryption", "[playfair]") {
   PlayfairCipher pc{"Cipher Key"};
   // N.B. Due to the sanitisation of the input text, the best we can get out
   //   is with the double letters seperated and 'Z' padded to even length.
-  REQUIRE( pc.applyCipher("ECSHSFQVDAUM", CipherMode::Decrypt) == "HELXLOWORLDZ");
+  REQUIRE( pc.applyCipher("ECSHFSVQADMU", CipherMode::Decrypt) == "HELXLOWORLDZ");
 }
